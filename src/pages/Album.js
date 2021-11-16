c25963ccd2d7c52c279ed40128583cfe36ed41b2
Fernanda9421 <fer.candrade21@gmail.com>
2021-11-16 10:56:51 -0300

    Cria lista de musicas do album selecionado - Requisito 7

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,14 +1,72 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artistName: '',
+      collectionName: '',
+      collectionImage: '',
+      tracks: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMusics();
+  }
+
+  fetchMusics = async () => {
+    const { match: { params: { id } } } = this.props;
+    const result = await getMusics(id);
+    this.setState({
+      artistName: result[0].artistName,
+      collectionName: result[0].collectionName,
+      collectionImage: result[0].artworkUrl100,
+    }, () => {
+      this.setState({
+        tracks: [...result],
+      });
+    });
+  }
+
   render() {
+    const { artistName, collectionImage, collectionName, tracks } = this.state;
+
     return (
       <div data-testid="page-album">
         <Header />
+        <main>
+          <img src={ collectionImage } alt={ collectionName } />
+          <h1 data-testid="album-name">{ collectionName }</h1>
+          <p data-testid="artist-name">{ artistName }</p>
+        </main>
+        {
+          tracks
+            .filter((track) => track.previewUrl !== undefined)
+            .map((track) => (
+              <MusicCard
+                key={ track.trackId }
+                trackName={ track.trackName }
+                previewUrl={ track.previewUrl }
+              />
+            ))
+        }
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+};
 
 export default Album;
