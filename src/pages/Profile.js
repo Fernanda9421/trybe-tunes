8b609b53e366e9c4efa56f58ec29e4bcaa0f506d
Fernanda9421 <fer.candrade21@gmail.com>
2021-11-18 07:10:33 -0300

    Cria exibiçao de perfil - Requisito 13

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,67 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoading: false,
+      image: '',
+      name: '',
+      email: '',
+      description: '',
+    };
+  }
+
+  componentDidMount() {
+    this.getUserLogin();
+  }
+
+  getUserLogin = async () => {
+    this.setState({
+      isLoading: true,
+    });
+    const user = await getUser();
+    this.setState({
+      isLoading: false,
+      name: user.name,
+      email: user.email,
+      description: user.description,
+      image: user.image,
+    });
+  }
+
   render() {
+    const { isLoading, name, email, description, image } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
+        {
+          isLoading
+            ? <Loading />
+            : (
+              <main>
+                <img data-testid="profile-image" src={ image } alt={ name } />
+                <Link
+                  to="/profile/edit"
+                >
+                  Editar perfil
+                </Link>
+                <section>
+                  <span>Nome:</span>
+                  <p>{ name }</p>
+                  <span>Email:</span>
+                  <p>{ email }</p>
+                  <span>Descrição:</span>
+                  <p>{ description }</p>
+                </section>
+              </main>
+            )
+        }
       </div>
     );
   }
